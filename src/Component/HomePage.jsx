d441af606e565669755fb78f7d4d4954a1e9efe5
Timsina <diwas.timsina@accenture.com>
2020-05-03 20:47:33 -0400

    Rabina please look at this

---
+++ src/Component/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from '../Component/HomePage'
+
+function HomePage ()  {
+    
+    return(
+
+        <div>
+          This is Children of Shangri-Lost HomePage
+        </div>
+    );
+}
+export default HomePage;
